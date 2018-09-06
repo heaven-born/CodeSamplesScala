842c69507a15f7f09e1c6b9db6c37a00648a60f7
heaven-born <bogdan.vakulenko@gmail.com>
2018-09-06 16:19:46 +0300

    added P32

--- src/main/scala/CodeSamplesArithmetic.scala
+++ src/main/scala/CodeSamplesArithmetic.scala
@@ -9,6 +9,15 @@ object CodeSamplesArithmetic {
       def isPrime: Boolean = if (n<=1) false else !(2 until n - 1).toStream.exists(n % _ == 0)
     }
 
+
+    //P32 (**) Determine the greatest common divisor of two positive integer numbers.
+
+    def gcd(a: Int, b: Int) = {
+      lazy val stream : Stream[(Int,Int)] =  (a,b) #:: stream.map{case (i,k)=>(k, i % k)}
+      stream.dropWhile(_._2!=0).head._1
+    }
+
+
   }
 
 }
